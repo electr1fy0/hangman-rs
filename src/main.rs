a9e90771d2c836226406abeb6e3da6b00134be15
electr1fy0 <electr1fy0@icloud.com>
2025-06-10 23:59:44 +0530

    implement ciphered text generation and guess input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rand::prelude::*;
 use std::env;
 use std::io;
+
 struct Word {
     data: String,
     length: usize,
@@ -15,10 +16,32 @@ fn generate_word(wlen: usize) -> String {
     word
 }
 
+fn generate_bricks(wlen: usize) -> String {
+    let mut word = String::with_capacity(wlen);
+    for _ in 0..wlen {
+        word.push('-');
+    }
+    word
+}
+
+// fn compare_chars(key: &str, guess: &str, bricks: &str) { [WIP]
+//     let wlen = guess.len();
+
+//     for i in wlen {
+//        if
+//     }
+// }
+
 fn main() {
     println!("Welcome to Hangman! Let's hang.🧵");
+
+    // prasing moves
+    println!("Enter number of moves.");
+    let mut moves: u8 = 0;
+    u8_input(&mut moves);
+    println!("{moves}");
     println!("Enter word length.");
-    // parsing w-len logic
+    // parsing w-len
     let mut length = String::new();
     io::stdin()
         .read_line(&mut length)
@@ -27,11 +50,33 @@ fn main() {
 
     // word gen
     let data = generate_word(length);
-    let visible = env::var("DEBUG").is_err();
+    let visible = env::var("DEBUG").is_ok();
 
+    dbg!(visible);
     let word = Word {
         data,
         length,
         visible,
     };
+
+    let mut guess = String::with_capacity(word.length);
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error reading guess");
+    dbg!(&guess);
+
+    let mut bricks = generate_bricks(word.length);
+    dbg!(&bricks);
+
+    // compare_chars(&word.data, &guess, &bricks); [WIP]
+}
+
+fn u8_input(num: &mut u8) {
+    let mut string_var = String::new();
+
+    io::stdin()
+        .read_line(&mut string_var)
+        .expect("Error reading string");
+
+    *num = string_var.trim().parse().expect("Error parsing string");
 }
