2b7b9b2238389724883e4764616ba6cdc84cfc68
electr1fy0 <electr1fy0@icloud.com>
2025-06-11 15:51:30 +0530

    refactor the project

---
+++ src/lib.rs
@@ -0,0 +1,96 @@
+use rand::prelude::*;
+use reqwest;
+use std::env;
+use std::io;
+
+pub struct Data {
+    pub secret: String,
+    pub guess: String,
+    pub bricks: String,
+    pub length: usize,
+    pub moves: u8,
+}
+
+pub fn config_data() -> Data {
+    // Read number of moves
+    println!("Enter number of moves.");
+    let mut moves: u8 = 0;
+    u8_input(&mut moves);
+    dbg!(&moves);
+
+    // Read input length
+    println!("Enter word length.");
+    let mut length = String::new();
+    io::stdin()
+        .read_line(&mut length)
+        .expect("Error reading word length");
+    let length: usize = length.trim().parse().expect("Error parsing word length");
+
+    // Get a word from the API
+    let secret = request_word(length).unwrap();
+    dbg!(&secret);
+    // Make the secret visible if debug is set as an environment variable
+    let visible = env::var("DEBUG").is_ok();
+    dbg!(visible); // Log visibility
+    if visible {
+        println!("{}", secret);
+    }
+    // Take the guess from stdin
+    println!("Enter your guess.");
+    let mut guess = String::with_capacity(length);
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Error reading guess");
+    guess = guess.trim().to_string();
+    dbg!(&guess);
+
+    // Construct the string of dashes
+    let mut bricks = generate_bricks(length);
+    dbg!(&bricks);
+
+    let data = Data {
+        length,
+        moves,
+        secret,
+        bricks,
+        guess,
+    };
+    data
+}
+
+fn u8_input(num: &mut u8) {
+    let mut string_var = String::new();
+
+    io::stdin()
+        .read_line(&mut string_var)
+        .expect("Error reading string");
+
+    *num = string_var.trim().parse().expect("Error parsing string");
+}
+
+pub fn generate_word(wlen: usize) -> String {
+    let mut word = String::with_capacity(wlen);
+    for _ in 0..word.capacity() {
+        word.push(rand::random_range(b'a'..b'z') as char);
+    }
+    word
+}
+
+pub fn request_word(wlen: usize) -> Result<String, Box<dyn std::error::Error>> {
+    let url = format!(
+        "https://random-word-api.vercel.app/api?words=1&length={}
+",
+        wlen
+    );
+
+    let res: Vec<String> = reqwest::blocking::get(url)?.json()?;
+    Ok(res[0].clone())
+}
+
+pub fn generate_bricks(wlen: usize) -> String {
+    let mut word = String::with_capacity(wlen);
+    for _ in 0..wlen {
+        word.push('-');
+    }
+    word
+}
--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,26 @@
+use hangman;
 use rand::prelude::*;
 use reqwest;
 use std::env;
 use std::io;
 
-struct Word {
-    data: String,
-    length: usize,
-    visible: bool,
-}
+// fn compare_chars(data: &mut Data) {
+//     // [WIP]
+//     let wlen = guess.len();
 
-fn generate_word(wlen: usize) -> String {
-    let mut word = String::with_capacity(wlen);
-    for _ in 0..word.capacity() {
-        word.push(rand::random_range(b'a'..b'z') as char);
-    }
-    word
-}
-fn request_word(wlen: usize) -> Result<String, Box<dyn std::error::Error>> {
-    let url = format!("https://random-word-api.herokuapp.com/word?length={}", wlen);
-    let res = reqwest::blocking::get(url)?.text()?;
+//     let mut chars: Vec<char> = bricks.chars().collect();
 
-    Ok(res)
-}
-
-fn generate_bricks(wlen: usize) -> String {
-    let mut word = String::with_capacity(wlen);
-    for _ in 0..wlen {
-        word.push('-');
-    }
-    word
-}
-
-fn compare_chars(key: &str, guess: &str, bricks: &str) {
-    // [WIP]
-    let wlen = guess.len();
-
-    let mut chars: Vec<char> = bricks.chars().collect();
-
-    for i in 0..wlen {
-        if key.chars().nth(i) == guess.chars().nth(i) {
-            println!("match");
-            chars[i] = guess.chars().nth(i).unwrap();
-        }
-    }
-}
+//     for i in 0..wlen {
+//         if key.chars().nth(i) == guess.chars().nth(i) {
+//             dbg!(&key.chars().nth(i));
+//             chars[i] = guess.chars().nth(i).unwrap();
+//         }
+//     }
+// }
 
 fn main() {
     println!("Welcome to Hangman! Let's hang.🧵");
+    let mut data = hangman::config_data();
 
-    // prasing moves
-    println!("Enter number of moves.");
-    let mut moves: u8 = 0;
-    u8_input(&mut moves);
-    println!("{moves}");
-    println!("Enter word length.");
-    // parsing w-len
-    let mut length = String::new();
-    io::stdin()
-        .read_line(&mut length)
-        .expect("Error reading word length");
-    let length: usize = length.trim().parse().expect("Error parsing word length");
-
-    // word gen
-    let data = request_word(length).unwrap();
-    let visible = env::var("DEBUG").is_ok();
-
-    dbg!(visible);
-
-    if visible {
-        println!("{}", data);
-    }
-    let word = Word {
-        data,
-        length,
-        visible,
-    };
-
-    let mut guess = String::with_capacity(word.length);
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Error reading guess");
-    dbg!(&guess);
-
-    let mut bricks = generate_bricks(word.length);
-    dbg!(&bricks);
-
-    compare_chars(&word.data, &guess, &bricks);
-}
-
-fn u8_input(num: &mut u8) {
-    let mut string_var = String::new();
-
-    io::stdin()
-        .read_line(&mut string_var)
-        .expect("Error reading string");
-
-    *num = string_var.trim().parse().expect("Error parsing string");
+    // compare_chars(&mut data);
 }
